ff5761b878f2516546a5a06c358f5ba01d0fb607
KsushaRychkova <49212558+KsushaRychkova@users.noreply.github.com>
2019-10-14 20:36:46 -0400

    Add files via upload

---
+++ ConductingPattern.java
@@ -0,0 +1,20 @@
+/* Mentored Research
+ * Student: Kseniya Rychkova
+ * Mentor: Dr. Salgian
+ * Project: Testing out the use of JFrame and JPanel to create 2D animations with Java.
+ */
+
+
+public class ConductingPattern {
+
+	
+	// class variables
+	private int bpMin; // beats per minute
+	private int bpBar; // beats per bar
+	
+	public ConductingPattern() {
+		
+	}
+	
+	
+}
---
+++ Main.java
@@ -0,0 +1,27 @@
+/* Mentored Research
+ * Student: Kseniya Rychkova
+ * Mentor: Dr. Salgian
+ * Project: Testing out the use of JFrame and JPanel to create 2D animations with Java.
+ */
+
+
+import java.awt.EventQueue;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*
+		EventQueue.invokeLater(() -> {
+			MyWindow window = new MyWindow();
+            window.setVisible(true);
+        });
+		*/
+		
+		MyWindow window = new MyWindow();
+		window.setVisible(true);
+			
+	}
+	
+}
